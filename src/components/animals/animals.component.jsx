0451af6608f153fda7de1ef8d23edb78d658dd40
Andres Alban <bethesdas101@gmail.com>
2020-10-31 23:26:21 -0500

    Adding Animal styles and html structure

--- src/components/animals/animals.component.jsx
+++ src/components/animals/animals.component.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 import './animals.styles.scss';
 
-const Animals = () => {
-    return (
-        <div>Animales</div>
+const Animals = (animals) => {    
+    const { name, imageUrl, specie } = animals;
+    
+    return (                    
+        <div className='animal-card'>
+            <div className='thumbnail' style={{backgroundImage: `url(${imageUrl})`}}/>
+            <div className='animal-info'>
+                <h2 className='name'>{name}</h2>
+                <p className='specie'>{specie}</p>
+            </div>
+            <div className='badge'>                
+                <p>Injured</p>
+                <div className='cross'></div>
+            </div>
+        </div>                
     );
 };
 
