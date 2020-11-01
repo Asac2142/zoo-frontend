8227c3ea0399d5d8c0c32c19849433fdd6eb9a9a
Andres Alban <bethesdas101@gmail.com>
2020-11-01 15:08:36 -0500

    Updating styles for Appjs component, adding classes for sections

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import Main from './components/main/main.component';
+import Header from './components/header/header.component';
+import Safe from './components/safe-animals/safe-animals.component';
+import ReportNewAnimal from './components/report-new-animal/report-new-animal.component';
 
 import './App.css';
-import Header from './components/header/header.component';
-import { Route, Switch } from 'react-router-dom';
-import Safe from './components/safe-animals/safe-animals.component';
 
 class App extends React.Component {
 
   render() {
     return (
       <div className='container'>
-        <header>
+        <header className='header'>
           <Header />
         </header>
-        <section>
+        <section className='main'>
           <Switch>
             <Route exact path='/' component={ Main } />
             <Route exact path='/safe' component={ Safe } />
+            <Route exact path='/report' component={ ReportNewAnimal } />
           </Switch>
         </section>
       </div>
