56a2c6d235964833e8d50a3bf2690f601c176c14
Andres Alban <bethesdas101@gmail.com>
2020-11-01 23:43:38 -0500

    Refactoring animals component, changing from function to class component

--- src/components/animals/animals.component.jsx
+++ src/components/animals/animals.component.jsx
@@ -1,23 +1,40 @@
 import React from 'react';
 
+import AnimalModal from '../modal-animal/modal-animal.component';
+
 import './animals.styles.scss';
 
-const Animals = (animals) => {    
-    const { name, imageUrl, specie } = animals;
-    
-    return (                    
-        <div className='animal-card'>
-            <div className='thumbnail' style={{backgroundImage: `url(${imageUrl})`}}/>
-            <div className='animal-info'>
-                <h2 className='name'>{name}</h2>
-                <p className='specie'>{specie}</p>
+class Animals extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false,
+            animal: this.props
+        }
+    }
+
+    render() {
+        const { name, imageUrl, specie } = this.state.animal;
+
+        return ( 
+            <div>
+                <div className='animal-card' onClick={() => this.setState({ show: !this.state.show }) }>
+                    <div className='thumbnail' style={{backgroundImage: `url(${imageUrl})`}}/>
+                    <div className='animal-info'>
+                        <h2 className='name'>{name}</h2>
+                        <p className='specie'>{specie}</p>
+                    </div>
+                    <div className='badge'>                
+                        <p>Injured</p>
+                        <div className='cross'></div>
+                    </div>
+                </div>
+                {
+                    this.state.show ? <AnimalModal animal={this.state.animal}/> : null
+                }
             </div>
-            <div className='badge'>                
-                <p>Injured</p>
-                <div className='cross'></div>
-            </div>
-        </div>                
-    );
+        );
+    }    
 };
 
 export default Animals;
