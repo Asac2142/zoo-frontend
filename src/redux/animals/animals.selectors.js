140b9428240a016b6b24e787cc48d4f310dadb6b
Andres Alban <bethesdas101@gmail.com>
2020-11-02 12:29:18 -0500

    Adding selector for healthy animals in animals redux

--- src/redux/animals/animals.selectors.js
+++ src/redux/animals/animals.selectors.js
@@ -1,14 +1,19 @@
 import { createSelector } from 'reselect';
 
-const selectInjuredAnimals = (state) => state.animals;
+const selectAllAnimals = (state) => state.animals;
 const selectSF = (state) => state.animals;
 
 export const selectAllInjuredAnimals = createSelector(
-    [selectInjuredAnimals], 
+    [selectAllAnimals], 
     (iAnimals) => iAnimals.animals.filter(animal => !animal.active)
 );
 
 export const selectSearchField = createSelector(
     [selectSF],
     (sf) => sf.searchField
+);
+
+export const selectAllHealthyAnimals = createSelector(
+    [selectAllAnimals],
+    (iAnimals) => iAnimals.animals.filter(animal => animal.active)
 );
