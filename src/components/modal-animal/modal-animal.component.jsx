b08af0b17ccec0c8d05764cfd32739f082faf4cb
Andres Alban <bethesdas101@gmail.com>
2020-11-02 18:40:28 -0500

    Updating handleSubmit for modal animal, adding update and delete functions, updating styles

--- src/components/modal-animal/modal-animal.component.jsx
+++ src/components/modal-animal/modal-animal.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { setHealthyAnimal } from '../../redux/animals/animals.actions';
+import { setHealthyAnimal, updateAnimal, deleteAnimal } from '../../redux/animals/animals.actions';
 
 import './modal-animal.styles.scss';
 
@@ -15,9 +15,20 @@ class AnimalModal extends React.Component {
         }        
     }
 
-    handleChange = (event) => {
+    handleChange = (event) => {                
         const { name, value } = event.target;
-        this.setState({ [name]: value });
+        const animal = {...this.state.animal, [name]: value};
+        this.setState({animal});
+    }
+
+    handleSubmit = (event) => {        
+        if (event.target.id === 'btn-update') {            
+            this.props.updateAnimal(this.state.animal);
+        } else {
+            this.props.deleteAnimal(this.state.animal);
+        }
+
+        this.setState({ show: !this.state.show });        
     }
 
     render() {
@@ -31,37 +42,37 @@ class AnimalModal extends React.Component {
                     <div 
                         className='profile-photo' 
                         style={{backgroundImage: `url(${this.state.animal.imageUrl})`}}/>
-                    <p>{this.state.animal.name.toUpperCase()}</p>
+                    <p>{name.toUpperCase()}</p>
                     <div className='close-icon' onClick={() => this.setState({ show: !this.state.show })}/>
-                    <div className='modal-header'>
+                    <form className='modal-header'>
                         <label>Family</label>
                         <div className='input-block'>
-                            <input type='text' name='family' value={family} onChange={this.handleChange}/>
+                            <input type='text' name='family' value={family} onChange={this.handleChange.bind(this)} required/>
                         </div>
     
                         <label>Specie</label>
                         <div className='input-block'>
-                            <input type='text' name='specie' value={specie} onChange={this.handleChange}/>
+                            <input type='text' name='specie' value={specie} onChange={this.handleChange} required/>
                         </div>
     
                         <label>Name</label>
                         <div className='input-block'>
-                            <input type='text' name='name' value={name} onChange={this.handleChange}/>
+                            <input type='text' name='name' value={name} onChange={this.handleChange} required/>
                         </div>
     
                         <label>Age</label>
                         <div className='input-block'>
-                            <input type='number' name='age' value={age} onChange={this.handleChange}/>
+                            <input type='number' name='age' value={age} onChange={this.handleChange} required/>
                         </div>
     
                         <label>Medical Status</label>
                         <div className='input-block'>
                             <input type='text' name='medical_status' value={medical_status} onChange={this.handleChange}/>
                         </div>
-                    </div>
+                    </form>
                     <div className='modal-footer'>
-                        <button className='btn-delete'>Delete</button>
-                        <button className='btn-update'>Update</button>
+                        <button id='btn-delete' className='btn-delete' onClick={this.handleSubmit}>Delete</button>
+                        <button id='btn-update' className='btn-update' onClick={this.handleSubmit}>Update</button>
                         <button 
                             className='btn-safe' 
                             onClick={() => {
@@ -80,7 +91,9 @@ class AnimalModal extends React.Component {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        setHealthy: (id) => dispatch(setHealthyAnimal(id))
+        setHealthy: (id) => dispatch(setHealthyAnimal(id)),
+        updateAnimal: (animal) => dispatch(updateAnimal(animal)),
+        deleteAnimal: (animal) => dispatch(deleteAnimal(animal))
     }
 };
 
