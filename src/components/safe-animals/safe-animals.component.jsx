7ac22c814c9cf7884d7f5d0e99b55bb8c8403660
Andres Alban <bethesdas101@gmail.com>
2020-11-02 12:30:10 -0500

    Updating safe components for healthy animals

--- src/components/safe-animals/safe-animals.component.jsx
+++ src/components/safe-animals/safe-animals.component.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import Animals from '../animals/animals.component';
+import { selectAllHealthyAnimals } from '../../redux/animals/animals.selectors';
 
 import './safe-animals.styles.scss';
 
-const Safe = () => {
+const Safe = ({ healthyAnimals }) => {
     return (
-        <div>Safe Animals!</div>
+        <div className='safe-container'>
+        {
+            healthyAnimals.length > 0 ? 
+                <div className='safe-container'>
+                    <h1>These animals are healthy!</h1>
+                    <div className='animal-container'>
+                        {
+                            healthyAnimals.map(({id, ...animals}) => {
+                            return (
+                                <Animals key={id} {...animals} id={id} />
+                            )
+                        })
+                        }
+                    </div>
+                </div> :
+                <h1>No animals here yet!</h1>
+        }
+        </div>
     )
 };
 
-export default Safe;
+const mapStateToProps = createStructuredSelector({
+    healthyAnimals: selectAllHealthyAnimals
+})
+
+export default connect(mapStateToProps, null)(Safe);
