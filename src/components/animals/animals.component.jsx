3310da87d23edbe8414343f2390cfa3ca4761c92
Andres Alban <bethesdas101@gmail.com>
2020-11-02 12:31:07 -0500

    Updating component with section of healthy animal, adding safe.png

--- src/components/animals/animals.component.jsx
+++ src/components/animals/animals.component.jsx
@@ -14,20 +14,35 @@ class Animals extends React.Component {
         }
     }
 
+    setShowModal = () => {
+        if (!this.state.animal.active) {
+            this.setState({ show: !this.state.show });
+        }
+    }
+
     render() {
         const { name, imageUrl, specie } = this.state.animal;        
         return ( 
             <div>
-                <div className='animal-card' onClick={() => this.setState({ show: !this.state.show }) }>
+                <div 
+                    className='animal-card' 
+                    onClick={() => this.setShowModal()}>
                     <div className='thumbnail' style={{backgroundImage: `url(${imageUrl})`}}/>
                     <div className='animal-info'>
                         <h2 className='name'>{name}</h2>
                         <p className='specie'>{specie}</p>
                     </div>
-                    <div className='badge'>                
-                        <p>Injured</p>
-                        <div className='cross'></div>
-                    </div>
+                    {
+                        !this.state.animal.active ?
+                        <div className='badge'>                
+                            <p>Injured</p>
+                            <div className='cross'/>
+                        </div> :
+                        <div className='badge-safe'>
+                            <p>Healthy</p>
+                            <div className='green-icon'/>
+                        </div>
+                    }
                 </div>
                 {
                     this.state.show ? <AnimalModal animal={this.state.animal} id={this.state.id}/> : null
