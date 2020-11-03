f64e5e62e36d49170ee0f16bea5bde586a9bebd5
Andres Alban <bethesdas101@gmail.com>
2020-11-02 20:14:47 -0500

    Adding responsive design for navigation bar on mobile devices

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,20 +3,30 @@ import { Link } from 'react-router-dom';
 
 import './header.styles.scss';
 
-const Header = () => {
-    return (
-        <div className='header-main'>
-            <Link className='logo-container' to='/'>
-                <div className='logo'></div>
-            </Link>
-            <div className='hamburguer-btn'/>
-            <div className='nav-container'> 
-                <Link className='option main' to='/report'>Report new animal</Link>
-                <Link className='option' to='/'>Medical attention</Link>
-                <Link className='option' to='/safe'>Recovered</Link>
+class Header extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            hidden: false
+        }
+    }
+
+    render() {
+        return (
+            <div className='header-main'>
+                <Link className='logo-container' to='/'>
+                    <div className='logo'></div>
+                </Link>
+                <div className='hamburguer-btn' onClick={() => this.setState({ hidden: !this.state.hidden })}/>
+                <div className='nav-container' style={{visibility: `${this.state.hidden ? 'visible' : 'hidden'}`}}>
+                    <Link className='option main' to='/report' onClick={() => this.setState({ hidden: !this.state.hidden })}>Report new animal</Link>
+                    <Link className='option' to='/' onClick={() => this.setState({ hidden: !this.state.hidden })}>Medical attention</Link>
+                    <Link className='option' to='/safe' onClick={() => this.setState({ hidden: !this.state.hidden })}>Recovered</Link>
+                </div>
+                               
             </div>
-        </div>
-    )
+        )
+    }
 };
 
 export default Header;
