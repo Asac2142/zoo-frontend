7cbb22af1539193ba31f3b5a17327edc1c9eb1e5
Andres Alban <bethesdas101@gmail.com>
2020-11-03 19:42:57 -0500

    Refactoring updateAnimal and setHealthyAnimal functions

--- src/redux/animals/animals.utils.js
+++ src/redux/animals/animals.utils.js
@@ -1,15 +1,16 @@
-export const setHealthyAnimal = (animals, id) => {
-    let animal = animals.filter(animl => animl.id === id);
+export const setHealthyAnimal = (animals, id) => {    
     let pos = animals.findIndex(animl => animl.id === id);
-
-    animal[0].active = true;
-    animals[pos] = animal[0];
+    if (pos >= 0) {
+        animals[pos] = {...animals[pos], active: true};
+    }
     return [...animals];
 };
 
 export const updateAnimal = (animals, animalToBeUpdated) => {
-    let pos = animals.findIndex(animal => animal.id === animalToBeUpdated.id);
-    animals[pos] = animalToBeUpdated;
+    let pos = animals.findIndex(animal => animal.id === animalToBeUpdated.id);    
+    if (pos >= 0) {
+        animals[pos] = animalToBeUpdated;
+    }
     return [...animals];
 };
 
